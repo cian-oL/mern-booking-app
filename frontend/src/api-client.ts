07376347cc9c5222cf49d4f4be2c32779713099f
mecastor <mecastor87@gmail.com>
2024-06-15 13:49:15 +0100

    Created hotel search request

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,7 +1,16 @@
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 
-import { HotelType } from "../../backend/src/shared/types";
+import { HotelSearchResponse, HotelType } from "../../backend/src/shared/types";
+
+export type SearchParams = {
+  destination?: string;
+  checkIn?: string;
+  checkOut?: string;
+  adultCount?: string;
+  childCount?: string;
+  page?: string;
+};
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
@@ -118,6 +127,28 @@ export const updateOneHotelById = async (formData: FormData) => {
 
   if (!response.ok) {
     throw new Error("Failed to update hotel");
+  }
+
+  return response.json();
+};
+
+export const searchHotels = async (
+  searchParams: SearchParams
+): Promise<HotelSearchResponse> => {
+  const queryParams = new URLSearchParams();
+  queryParams.append("destination", searchParams.destination || "");
+  queryParams.append("checkIn", searchParams.checkIn || "");
+  queryParams.append("checkOut", searchParams.checkOut || "");
+  queryParams.append("adultCount", searchParams.adultCount || "");
+  queryParams.append("childCount", searchParams.childCount || "");
+  queryParams.append("page", searchParams.page || "");
+
+  const response = await fetch(
+    `${API_BASE_URL}/api/hotels/search?${queryParams}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Error retrieving hotels");
   }
 
   return response.json();
